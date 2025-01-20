a2fb7fd6918b991599679c1cb35e0abb916db4ab
Ashish48Maurya <maurya.48.ashish@gmail.com>
2025-01-20 21:35:44 +0530

    db setup done

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import express from "express"
-import Connection from "./db.js";
+const express  = require("express") 
 const app = express();
 app.listen(3000, async () => {
-    await Connection();
   console.log("Server is running on port 3000");
 });
